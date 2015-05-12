3176b7e4070208f3da67505a63ad65db8c792e7b
Tiagofernandes <tiagofernandesp@gmail.com>
2015-05-12 03:54:20 +0100

    Static Index, Angular dir and controller

---
+++ app/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('escalasApp', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/escalas',
+            {
+                controller: 'EscalasController',
+                templateUrl: 'app/views/panelEscalas.html'
+            })
+        .otherwise({ redirectTo: '/escalas' });
+}]);
+
+
---
+++ js/myJS.js
@@ -0,0 +1,10 @@
+$('#dp3').datepicker({
+	format: "dd/mm/yyyy",
+	autoclose: true,
+	language: "pt"
+});
+$('#dp2').datepicker({
+	format: "dd/mm/yyyy",
+	autoclose: true,
+	language: "pt"
+});
